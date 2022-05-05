f96de6f604b33793ca3257358e4afe4cb8a9dcbb
RudhyDev <rudhy.pereira@gmail.com>
2022-05-04 22:03:33 -0300

    feat: create feedback post route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,24 @@
+import { prisma } from "./prisma";
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-const port = 3333
+app.post("/feedbacks", async(req, res) => {
+	const { type, comment, screeshot } = req.body;
+
+	const feedback = await prisma.feedback.create({
+		data: {
+			type,
+			comment,
+			screeshot,
+		},
+	});
+
+	return res.status(201).json({data: feedback})
+});
+
+const port = 3333;
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
-
-app.get('/users', (req, res)=>{
-  return res.send('Hello World');
-})
