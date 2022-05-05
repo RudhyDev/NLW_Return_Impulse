a167970d544534b2bce0e9b4ca77c1c7463e6fc6
RudhyDev <rudhy.pereira@gmail.com>
2022-05-04 22:35:55 -0300

    feat: implement e-mail sending using mailtrap (e-mail sandbox service)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,20 @@
 import { prisma } from "./prisma";
 import express from "express";
+import nodemailer from "nodemailer";
 
 const app = express();
 app.use(express.json());
 
-app.post("/feedbacks", async(req, res) => {
+const transport = nodemailer.createTransport({
+	host: "smtp.mailtrap.io",
+	port: 2525,
+	auth: {
+		user: "175395f7ddef1a",
+		pass: "21d0c6ef2efd44",
+	},
+});
+
+app.post("/feedbacks", async (req, res) => {
 	const { type, comment, screeshot } = req.body;
 
 	const feedback = await prisma.feedback.create({
@@ -15,7 +25,19 @@ app.post("/feedbacks", async(req, res) => {
 		},
 	});
 
-	return res.status(201).json({data: feedback})
+	await transport.sendMail({
+		from: "Feedget <rudhy_nlw_return@feedget.com",
+		to: "Rudhy Pereira <rudhydev@gmail.com>",
+		subject: "Novo feedback",
+		html: [
+			`<div style="font-family: sans-serif; font-size: 16px; color: #222">`,
+			`<p>Tipo do Feedback: ${type}</p>`,
+			`<p>Comentário: ${comment}</p>`,
+			`</div>`,
+		].join("\n"),
+	});
+
+	return res.status(201).json({ data: feedback });
 });
 
 const port = 3333;
